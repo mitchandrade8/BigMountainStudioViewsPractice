9eb40aa53cee90b72bf80300147fe624a541c0bc
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-09-25 12:41:50 -0600

    Initial Commit

---
+++ BigMountainStudioViewsPractice/BigMountainStudioViewsPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigMountainStudioViewsPracticeApp.swift
+//  BigMountainStudioViewsPractice
+//
+//  Created by Mitch Andrade on 9/25/23.
+//
+
+import SwiftUI
+
+@main
+struct BigMountainStudioViewsPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
